0c5cbc545f157d07167030a4fcd4d98e0cdb1753
hana-jung <hn.jung@sparta-devcamp.com>
2023-10-20 16:19:13 +0900

    feat: create axios instance
    - 카카오 로그인을 위한 axios instance를 만듭니다
    
    by hana-jung

--- src/axios/axios.tsx
+++ src/axios/axios.tsx
@@ -82,3 +82,46 @@ loginInstance.interceptors.response.use(
     return Promise.reject(error)
   }
 )
+
+export const kakakoInstance = axios.create({
+  baseURL: env.NEXT_PUBLIC_API_BASE_URL,
+  headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" },
+})
+kakakoInstance.interceptors.request.use((config) => {
+  // const token = Cookies.get("accessToken")
+  const reqId = uuidv4()
+  config.headers["x-request-id"] = reqId
+
+  return config
+})
+
+kakakoInstance.interceptors.response.use(
+  function (response) {
+    // 값이 정상적으로 들어왔을 때(200번대 응답)
+    if (response.data.accessToken) {
+      Cookies.set("accessToken", response.data.accessToken)
+    }
+    if (response.data.refreshToken) {
+      Cookies.set("refreshToken", response.data.refreshToken)
+    }
+    if (response.data.user) {
+      Cookies.set("user", response.data.user)
+    }
+    console.log("성공")
+    return response
+  },
+
+  async function (error) {
+    if (error.response) {
+      // 에러일 경우 처리(200번대 응답이 아닐 경우)
+      // router.push("/member-redirect")
+      //
+      console.log("이미 가입")
+      return
+    }
+
+    //if(엑세스 토큰이 없을 때 혹은 만료되었을 때 )
+
+    return Promise.reject(error)
+  }
+)
